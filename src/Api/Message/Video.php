295ade54660705073fbd70fefaab4a496279828f
1cmobile <kharchenkodenis@yandex.ru>
2017-08-19 10:56:38 +0300

    first

---
+++ src/Api/Message/Video.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Viber\Api\Message;
+
+use Viber\Api\Message;
+
+/**
+ * Video as message
+ *
+ * @author Novikov Bogdan <hcbogdan@gmail.com>
+ */
+class Video extends Message
+{
+    /**
+     * URL of the video (MP4, H264)
+     *
+     * @var string
+     */
+    protected $media;
+
+    /**
+     * Size of the video in bytes
+     *
+     * @var integer
+     */
+    protected $size;
+
+    /**
+     * Video duration in seconds
+     *
+     * @var integer
+     */
+    protected $duration;
+
+    /**
+     * URL of a reduced size image (JPEG)
+     *
+     * @var string
+     */
+    protected $thumbnail;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return Type::VIDEO;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toArray()
+    {
+        return array_merge(parent::toArray(), [
+            'media' => $this->getMedia(),
+            'size' => $this->getSize(),
+            'duration' => $this->getDuration(),
+            'thumbnail' => $this->getThumbnail()
+        ]);
+    }
+
+    /**
+     * Get the value of URL of the video (MP4, H264)
+     *
+     * @return string
+     */
+    public function getMedia()
+    {
+        return $this->media;
+    }
+
+    /**
+     * Set the value of URL of the video (MP4, H264)
+     *
+     * @param string media
+     *
+     * @return self
+     */
+    public function setMedia($media)
+    {
+        $this->media = $media;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Size of the video in bytes
+     *
+     * @return integer
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * Set the value of Size of the video in bytes
+     *
+     * @param integer size
+     *
+     * @return self
+     */
+    public function setSize($size)
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Video duration in seconds
+     *
+     * @return integer
+     */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Set the value of Video duration in seconds
+     *
+     * @param integer duration
+     *
+     * @return self
+     */
+    public function setDuration($duration)
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of URL of a reduced size image (JPEG)
+     *
+     * @return string
+     */
+    public function getThumbnail()
+    {
+        return $this->thumbnail;
+    }
+
+    /**
+     * Set the value of URL of a reduced size image (JPEG)
+     *
+     * @param string thumbnail
+     *
+     * @return self
+     */
+    public function setThumbnail($thumbnail)
+    {
+        $this->thumbnail = $thumbnail;
+
+        return $this;
+    }
+
+    public function isValid()
+    {
+        if (!($this->getMedia() && $this->getSize())) {
+            return false;
+        }
+
+        if ($this->getDuration()&&((int) $this->getDuration())>180) {
+            return false;
+        }
+        //todo сделать проверку данного поля
+        // if ($this->getThumbnail()&&( $this->getThumbnail)) {
+        //     return false;
+        // }
+        return true;
+    }
+}
